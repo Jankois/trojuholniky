3510760662dee6adf4926478371ee62f8b0d3ff5
Jankois <72013823+Jankois@users.noreply.github.com>
2021-09-30 13:24:24 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Získavanie výšky trojuholníkov od použivaťela
+        System.out.print("Zadaj číslo:");
+        Scanner input_scan = new Scanner(System.in);
+        String input = input_scan.nextLine();
+        int vyska = parseInt(input);
+
+        //Printovanie originálneho trojuholníka
+        for (int i = 0; i <= vyska; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Oddelenie trojuholníkov
+        System.out.println();
+        System.out.println("|----|");
+        System.out.println();
+
+        //Printovanie mirrornut0ho trojuholníka
+        for (int i = 1; i <= vyska; i++){
+            for (int j = 1; j <= vyska - i; j++){
+                System.out.print(" ");
+            }
+            for (int y = 0; y < i; y++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Oddelenie trojuholníkov
+        System.out.println();
+        System.out.println("|----|");
+        System.out.println();
+
+        //Printovanie flipnutého trojuholníka
+        for (int i = 0; i <= vyska; i++){
+            for (int j = 0; j < vyska - i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Oddelenie trojuholníkov
+        System.out.println();
+        System.out.println("|----|");
+        System.out.println();
+
+        //Printovanie trojuholníka otočené
+        for (int i = 1; i <= vyska; i++){
+            for (int j = 1; j <= i - 1; j++){
+                System.out.print(" ");
+            }
+            for (int y = 0; y <= vyska - i; y++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
